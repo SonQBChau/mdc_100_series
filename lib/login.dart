304db7790b4afcf8a9e3121480d4d827f25974bf
Son Chau <soncqb@gmail.com>
2019-05-03 13:13:29 -0500

    add controller to button

--- lib/login.dart
+++ lib/login.dart
@@ -21,6 +21,9 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +45,7 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Wrap Password with AccentColorOverride (103)
             // TODO: Add TextField widgets (101)
             TextField(
+              controller: _usernameController,
               decoration: InputDecoration(
                 filled: true,
                 labelText: 'Username',
@@ -51,6 +55,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 12,
             ),
             TextField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 filled: true,
                 labelText: 'Password',
@@ -60,8 +65,14 @@ class _LoginPageState extends State<LoginPage> {
             // TODO: Add button bar (101)
             ButtonBar(
               children: <Widget>[
-                FlatButton(child: Text('CANCEL'), onPressed: () {}),
-                RaisedButton(child: Text('NEXT'), onPressed: () {}),
+                FlatButton(child: Text('CANCEL'), onPressed: () {
+                  _usernameController.clear();
+                  _passwordController.clear();
+                }),
+                RaisedButton(child: Text('NEXT'), onPressed: () {
+                  _usernameController.clear();
+                  _passwordController.clear();
+                }),
               ],
             ),
           ],
